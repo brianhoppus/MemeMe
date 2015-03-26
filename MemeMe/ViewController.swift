90ac4d863489819a1e0457979744cb4e624dd78b
Brian Saunders <brian.saunders84@gmail.com>
2015-03-25 20:29:27 -0700

    Add outlets for imageView and cameraButton

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
