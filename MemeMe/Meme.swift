707c9da3ae15f1291264bfa9ab8f26dd8937f215
Brian Saunders <brian.saunders84@gmail.com>
2015-03-28 15:21:58 -0700

    Add new Meme class

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,27 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Brian Saunders on 3/28/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Meme {
+    var topText = String()
+    var bottomText = String()
+    var originalImage = UIImage()
+    var memedImage = UIImage()
+    
+    init() {
+        
+    }
+    
+    init(topText: String, bottomText: String, originalImage: UIImage) {
+        self.topText = topText
+        self.bottomText = bottomText
+        self.originalImage = originalImage
+    }
+}
