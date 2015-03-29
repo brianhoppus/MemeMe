d2d96489f86b308cfd943d0eac1165b61a8f8024
Brian Saunders <brian.saunders84@gmail.com>
2015-03-29 09:52:26 -0700

    Add MemeTableViewController and MemeCollectionViewController; Access with cancel button

---
+++ MemeMe/MemeCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Brian Saunders on 3/29/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UIViewController {
+
+}
---
+++ MemeMe/MemeTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemeTableViewController.swift
+//  MemeMe
+//
+//  Created by Brian Saunders on 3/29/15.
+//  Copyright (c) 2015 Brian Saunders. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UIViewController {
+
+}
